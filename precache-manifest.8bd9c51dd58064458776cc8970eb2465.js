c303618b1e4e600f8fbfa95f485c16d0ef56ac42
Your Name <vikash@csschopper.com>
2021-03-06 15:15:42 +0530

    Updates

---
+++ precache-manifest.8bd9c51dd58064458776cc8970eb2465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6926797703ff89a6f8dc472f2ac2cb99",
+    "url": "/realTimeChatApp/index.html"
+  },
+  {
+    "revision": "433270e8a894f5f222b1",
+    "url": "/realTimeChatApp/static/css/main.3551c3e9.chunk.css"
+  },
+  {
+    "revision": "827810199114de72fb4a",
+    "url": "/realTimeChatApp/static/js/2.0502e954.chunk.js"
+  },
+  {
+    "revision": "b38dbb22c32ecdedb4e97ae04f4976bc",
+    "url": "/realTimeChatApp/static/js/2.0502e954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433270e8a894f5f222b1",
+    "url": "/realTimeChatApp/static/js/main.0bca79d1.chunk.js"
+  },
+  {
+    "revision": "5a8d9b2b561dcf7db161",
+    "url": "/realTimeChatApp/static/js/runtime-main.b6ef1f49.js"
+  }
+]);
